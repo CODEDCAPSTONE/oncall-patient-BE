606ea5c53e288fc59e8ba8b4cde1c8f78e42d167
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-07-23 18:01:44 +0300

    fixes

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,9 @@
 import app from "./app";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { connectDB } from "./config/DataBase";
 
 dotenv.config();
-
-const connectDB = async () => {
-  try {
-    const connection = await mongoose.connect(process.env.MONGODB_URI || "");
-    console.log("✅ Connected to MongoDB: OnCall Full Stack");
-  } catch (error) {
-    console.error("❌ Error connecting to MongoDB:", error);
-    process.exit(1);
-  }
-};
 
 const PORT = process.env.PORT
 
